a0affed758a303dc6e74e8c8eda161f5bc98fbe9
4cteru <yoshiteru.kawamata@mixi.co.jp>
2023-08-31 17:50:56 +0900

    quit from mixi

--- scripts/spanner-er-docker.sh
+++ scripts/spanner-er-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --entrypoint '' -v `pwd`:/go/src/github.com/nakatamixi/spanner-er --workdir="/go/src/github.com/nakatamixi/spanner-er" nakatamixi/spanner-er  /bin/sh -c "/bin/spanner-er $*"
+docker run --rm --entrypoint '' -v `pwd`:/go/src/github.com/nktks/spanner-er --workdir="/go/src/github.com/nakatamixi/spanner-er" nakatamixi/spanner-er  /bin/sh -c "/bin/spanner-er $*"
